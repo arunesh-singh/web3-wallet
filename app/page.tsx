c381ba2987bb9cec2abc4c2c1e08f27b5d29ca11
arunesh-singh <aruneshsingh433@gmail.com>
2024-08-18 10:04:53 +0530

    project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,127 @@
+"use client";
+
+import { ToggleTheme } from "@/components/toogle-theme";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { DashboardIcon, SunIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
+import nacl from "tweetnacl";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { derivePath, getMasterKeyFromSeed } from "ed25519-hd-key";
+import { Keypair } from "@solana/web3.js";
+import { use, useCallback, useEffect, useState } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+type Wallet = {
+  seed: string[];
+  secret: string;
+  publicKey: string;
+};
+
 export default function Home() {
+  const [mnemonicWords, setMnemonicWords] = useState<string[]>([]);
+  const [vault, setVault] = useState<Wallet[] | null>(null);
+
+  // console.log("mnemonicWords: ", mnemonicWords);
+  console.log("vault: ", vault);
+
+  const generateWallet = useCallback((i: number) => {
+    const mnemonic = generateMnemonic();
+
+    setMnemonicWords(mnemonic.split(" "));
+    const seed = mnemonicToSeedSync(mnemonic);
+
+    const path = `m/44'/501'/${i}'/0'`;
+
+    // from this derived seed we can get modified mnemonic
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+
+    const keypair = Keypair.fromSecretKey(secret);
+
+    const privateKey = Buffer.from(keypair.secretKey).toString("base64");
+    // console.log("privateKey: ", privateKey);
+    const publicKey = keypair.publicKey.toBase58();
+
+    setVault((prev) => [
+      ...(prev || []),
+      {
+        seed: mnemonic.split(" "),
+        secret: privateKey,
+        publicKey: publicKey,
+      },
+    ]);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center py-24 md:px-64 lg:px-96 w-full">
+      <nav className="sticky top-0 flex justify-between items-center w-full">
+        <div className="flex item-center justify-center p-4 border rounded-full">
+          <DashboardIcon className="h-[2rem] w-[2rem]  " />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <ToggleTheme />
+      </nav>
+      <section className="w-full space-y-4">
+        <h1 className="text-[4rem] font-bold leading-10 mt-8">Wallet</h1>
+        <h5 className="text-2xl ">
+          A personal Web-3 Wallet for Harkirat&apos;s cohort 3.0 assignment.
+        </h5>
+        <div className="flex w-full gap-8">
+          <Input
+            placeholder="Enter your secret phrase (or leave black to generate)"
+            className="w-full "
+          />
+          <Button
+            onClick={() => generateWallet(0)}
+            className="h-14 text-lg rounded-xl">
+            Add Wallet
+          </Button>
+        </div>
+        <Accordion type="single" collapsible className="w-full space-y-8">
+          {vault?.map((wallet, i) => (
+            <AccordionItem key={i} value={`item-${i}`}>
+              <AccordionTrigger>
+                <h3 className="text-2xl font-bold">Wallet {i + 1}</h3>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="space-y-4">
+                  <div className="space-y-2 ">
+                    <h4 className="text-xl font-bold">Public Key</h4>
+                    <h1 className="bg-background p-2 rounded-lg text-base">
+                      {wallet.publicKey}
+                    </h1>
+                  </div>
+                  <div className="space-y-2 ">
+                    <h4 className="text-xl font-bold">Private Key</h4>
+                    <p className="bg-background p-2 rounded-lg text-base break-normal">
+                      {wallet.secret}
+                    </p>
+                  </div>
+                  <div className="space-y-2">
+                    <h4 className="text-xl font-bold">Seed Phrase</h4>
+                    <div className="grid gap-2 grid-cols-4 ">
+                      {wallet.seed.map((word, i) => (
+                        <div
+                          key={i}
+                          className="bg-background p-2 flex items-center justify-center rounded-lg">
+                          {word}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </section>
     </main>
   );
 }
